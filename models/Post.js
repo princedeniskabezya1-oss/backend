7f8506a2fac1a8ff3fa3ad43a61ee4cdb1fc9377
admin <admin@admins-iMac.local>
2026-02-16 03:13:23 +0800

    Updated Post model to support media and author structure

--- models/Post.js
+++ models/Post.js
@@ -1,34 +1,54 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  employerId: {
+const PostSchema = new mongoose.Schema({
+
+  /*
+  ==========================================
+  AUTHOR
+  ==========================================
+  */
+  author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true
   },
 
+  /*
+  ==========================================
+  CONTENT
+  ==========================================
+  */
   content: {
-    type: String
+    type: String,
+    trim: true
   },
 
-  image: {
-    type: String
+  /*
+  ==========================================
+  MEDIA (IMAGE OR VIDEO)
+  ==========================================
+  */
+  mediaUrl: {
+    type: String,
+    default: null
   },
 
-  video: {
-    type: String
+  mediaType: {
+    type: String,
+    enum: ["image", "video", null],
+    default: null
   },
 
+  /*
+  ==========================================
+  ENGAGEMENT
+  ==========================================
+  */
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
-  }],
-
-  isProContent: {
-    type: Boolean,
-    default: false
-  }
+  }]
 
 }, { timestamps: true });
 
-module.exports = mongoose.model("Post", postSchema);
+module.exports = mongoose.model("Post", PostSchema);
