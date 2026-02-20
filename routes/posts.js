cfe244e812f48001bad92fb5b04ab9012c8df956
admin <admin@admins-iMac.local>
2026-02-21 06:58:51 +0800

    Debug cloudinary error

--- routes/posts.js
+++ routes/posts.js
@@ -72,11 +72,13 @@ router.get("/", auth, async (req, res) => {
 
     res.json(posts);
 
-  } catch (err) {
-    console.error("FEED ERROR:", err);
-    res.status(500).json({ message: err.message });
-  }
-});
+ } catch (err) {
+  console.error("CREATE POST ERROR FULL:", err);
+  res.status(500).json({
+    error: err.message,
+    stack: err.stack
+  });
+}
 
 /* =====================================================
    LIKE / UNLIKE
