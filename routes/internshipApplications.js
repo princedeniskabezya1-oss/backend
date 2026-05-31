e33a15ac04656996755cfde13190d184bcc984b1
princedeniskabezya1-oss <princedeniskabezya1@gmail.com>
2026-05-31 19:47:50 +0800

    Update internshipApplications.js

--- routes/internshipApplications.js
+++ routes/internshipApplications.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const InternshipApplication = require("../models/InternshipApplication");
-const Opportunity = require("../models/Opportunity");
+const Opportunity = require("../models/SchoolOpportunity");
 
 let auth = null;
 
