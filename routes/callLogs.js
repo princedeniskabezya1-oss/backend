17b2da69badb193a00a75406ae4c2a49b32d511f
princedeniskabezya1-oss <princedeniskabezya1@gmail.com>
2026-06-06 14:27:52 +0800

    Update callLogs.js

--- routes/callLogs.js
+++ routes/callLogs.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const CallLog = require("../models/CallLog");
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
