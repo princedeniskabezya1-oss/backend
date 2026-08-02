f129ff66b237a058d1d450c5ad78e564858ea352
princedeniskabezya1-oss <princedeniskabezya1@gmail.com>
2026-08-03 04:01:58 +0800

    Update QuizSubmission.js

--- models/QuizSubmission.js
+++ models/QuizSubmission.js
@@ -1,91 +1,181 @@
 const mongoose = require("mongoose");
 
-const quizAnswerSchema = new mongoose.Schema(
+
+const quizSubmissionAnswerSchema =
+  new mongoose.Schema(
+    {
+      questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+      },
+
+      answer: {
+        type: String,
+        trim: true,
+        default: ""
+      },
+
+      isCorrect: {
+        type: Boolean,
+        default: false
+      },
+
+      pointsEarned: {
+        type: Number,
+        min: 0,
+        default: 0
+      },
+
+      pointsPossible: {
+        type: Number,
+        min: 0,
+        default: 0
+      }
+    },
+    {
+      _id: false
+    }
+  );
+
+
+const quizSubmissionSchema =
+  new mongoose.Schema(
+    {
+      schoolId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+      },
+
+      classId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Class",
+        required: true,
+        index: true
+      },
+
+      quizId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Quiz",
+        required: true,
+        index: true
+      },
+
+      studentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+      },
+
+      attemptNumber: {
+        type: Number,
+        min: 1,
+        required: true
+      },
+
+      answers: {
+        type: [quizSubmissionAnswerSchema],
+        default: []
+      },
+
+      score: {
+        type: Number,
+        min: 0,
+        default: 0
+      },
+
+      totalPoints: {
+        type: Number,
+        min: 0,
+        default: 0
+      },
+
+      percentage: {
+        type: Number,
+        min: 0,
+        max: 100,
+        default: 0,
+        index: true
+      },
+
+      passed: {
+        type: Boolean,
+        default: false,
+        index: true
+      },
+
+      status: {
+        type: String,
+        enum: [
+          "submitted",
+          "reviewed"
+        ],
+        default: "submitted",
+        index: true
+      },
+
+      startedAt: {
+        type: Date,
+        default: null
+      },
+
+      submittedAt: {
+        type: Date,
+        default: Date.now,
+        index: true
+      },
+
+      timeSpentSeconds: {
+        type: Number,
+        min: 0,
+        default: 0
+      }
+    },
+    {
+      timestamps: true
+    }
+  );
+
+
+/*
+  A student can submit multiple attempts, but the same attempt
+  number cannot be created twice for the same quiz.
+*/
+quizSubmissionSchema.index(
   {
-    questionId: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-    },
-
-    answer: {
-      type: String,
-      default: "",
-    },
-
-    isCorrect: {
-      type: Boolean,
-      default: false,
-    },
-
-    pointsEarned: {
-      type: Number,
-      default: 0,
-    },
+    quizId: 1,
+    studentId: 1,
+    attemptNumber: 1
   },
-  { _id: false }
+  {
+    unique: true
+  }
 );
 
-const quizSubmissionSchema = new mongoose.Schema(
-  {
-    schoolId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      index: true,
-    },
 
-    classId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Class",
-      required: true,
-      index: true,
-    },
+quizSubmissionSchema.index({
+  classId: 1,
+  studentId: 1,
+  submittedAt: -1
+});
 
-    quizId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Quiz",
-      required: true,
-      index: true,
-    },
 
-    studentId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-      index: true,
-    },
+quizSubmissionSchema.index({
+  quizId: 1,
+  submittedAt: -1
+});
 
-    answers: {
-      type: [quizAnswerSchema],
-      default: [],
-    },
 
-    score: {
-      type: Number,
-      default: 0,
-    },
+quizSubmissionSchema.index({
+  schoolId: 1,
+  submittedAt: -1
+});
 
-    totalPoints: {
-      type: Number,
-      default: 0,
-    },
 
-    percentage: {
-      type: Number,
-      default: 0,
-    },
-
-    passed: {
-      type: Boolean,
-      default: false,
-    },
-
-    submittedAt: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  { timestamps: true }
-);
-
-module.exports = mongoose.model("QuizSubmission", quizSubmissionSchema);
+module.exports =
+  mongoose.model(
+    "QuizSubmission",
+    quizSubmissionSchema
+  );
