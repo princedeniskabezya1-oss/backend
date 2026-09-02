20660e5e386771174584f54aa621eba96f88fb73
princedeniskabezya1-oss <princedeniskabezya1@gmail.com>
2026-09-02 11:00:17 +0800

    Add metadata support for Story notifications

--- models/Notification.js
+++ models/Notification.js
@@ -38,6 +38,10 @@ const notificationSchema = new mongoose.Schema({
   link: {
     type: String
   },
+  metadata: {
+    type: mongoose.Schema.Types.Mixed,
+    default: undefined
+  },
   read: {
     type: Boolean,
     default: false
@@ -46,5 +50,6 @@ const notificationSchema = new mongoose.Schema({
 
 notificationSchema.index({ user:1, read:1, createdAt:-1 });
 notificationSchema.index({ user:1, type:1, createdAt:-1 });
+notificationSchema.index({ user:1, type:1, "metadata.storyId":1, sender:1, createdAt:-1 });
 
 module.exports = mongoose.model("Notification", notificationSchema);
