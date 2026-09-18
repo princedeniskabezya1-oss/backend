a1257d8c58ef0eec4ede4f7c26a0035c07007875
princedeniskabezya1-oss <princedeniskabezya1@gmail.com>
2026-09-18 18:44:35 +0800

    Remove obsolete 250 MB post upload message

--- routes/posts.js
+++ routes/posts.js
@@ -827,9 +827,8 @@ function normalizeDirectMedia(value) {
 function receivePostMedia(req, res, next) {
   upload.postMedia.array("media", 10)(req, res, error => {
     if (!error) return next();
-    const message = error.code === "LIMIT_FILE_SIZE"
-      ? "Each image or video must be no larger than 250 MB."
-      : error.code === "LIMIT_FILE_COUNT" || error.code === "LIMIT_UNEXPECTED_FILE"
+    const message =
+      error.code === "LIMIT_FILE_COUNT" || error.code === "LIMIT_UNEXPECTED_FILE"
         ? "Choose up to 10 images or videos using the media field."
         : error.message;
     res.status(400).json({ message });
